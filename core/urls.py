39e629ff5569c64b4104ee51a0e3a8898bdb6ecb
Nhat Tai NGUYEN <nhattai.nguyen88@gmail.com>
2020-07-15 19:07:18 +0200

    Add item index view

---
+++ core/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import (
+    item_list
+)
+
+app_name = "core"
+
+urlpatterns = [
+    path("", item_list, name="item-index")
+]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Item
 
 # Create your views here.
+def item_list(request):
+    template = "items/index.html"
+    queryset = Item.objects.all()
+    context = {
+        "items": queryset
+    }
+    return render(request, template, context)
