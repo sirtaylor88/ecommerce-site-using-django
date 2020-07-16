1371ab8b980de30e43ce76508d9e75d9a98cd1a3
Nhat Tai NGUYEN <nhattai.nguyen88@gmail.com>
2020-07-16 09:26:52 +0200

    Add item discount price

--- core/models.py
+++ core/models.py
@@ -17,11 +17,12 @@ LABEL_CHOICES = (
 
 # Create your models here.
 class Item(models.Model):
-    title         = models.CharField(max_length=120)
-    price         = models.DecimalField(decimal_places=2, max_digits=10)
-    category      = models.CharField(choices = CATEGORY_CHOICES, max_length=2)
-    label         = models.CharField(choices = LABEL_CHOICES, max_length=1)
-    slug          = models.SlugField()
+    title           = models.CharField(max_length=120)
+    price           = models.DecimalField(decimal_places=2, max_digits=10)
+    discount_price  = models.DecimalField(decimal_places=2, max_digits=10, blank=True, null=True)
+    category        = models.CharField(choices = CATEGORY_CHOICES, max_length=2)
+    label           = models.CharField(choices = LABEL_CHOICES, max_length=1)
+    slug            = models.SlugField()
 
     def __str__(self):
         return self.title
