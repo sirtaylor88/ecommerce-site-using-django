40242639f6c4dfc101ad440ec79edbfdb32ca9e7
Nhat Tai NGUYEN <nhattai.nguyen88@gmail.com>
2020-07-16 07:55:06 +0200

    Add Category and Label choices

--- core/models.py
+++ core/models.py
@@ -1,10 +1,25 @@
 from django.conf import settings
 from django.db import models
 
+
+CATEGORY_CHOICES = (
+    ("S", "Shirt"),
+    ("SW", "Sport wear"),
+    ("OW", "Outwear"),
+)
+
+LABEL_CHOICES = (
+    ("P", "primary"),
+    ("S", "secondary"),
+    ("D", "danger"),
+)
+
 # Create your models here.
 class Item(models.Model):
     title         = models.CharField(max_length=120)
     price         = models.DecimalField(decimal_places=2, max_digits=10)
+    category      = models.CharField(choices = CATEGORY_CHOICES, max_length=2)
+    label         = models.CharField(choices = LABEL_CHOICES, max_length=1)
 
     def __str__(self):
         return self.title
