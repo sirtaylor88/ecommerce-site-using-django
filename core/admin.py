a94bbbf30b330bea65bceb7a8555692a29d31a57
Nhat Tai NGUYEN <nhattai.nguyen88@gmail.com>
2020-07-20 16:12:10 +0200

    Add column Ordered to Order view in Admin

--- core/admin.py
+++ core/admin.py
@@ -2,7 +2,11 @@ from django.contrib import admin
 from .models import Item, OrderItem, Order, Payment
 
 # Register your models here.
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ["user", "ordered"]
+
 admin.site.register(Item)
 admin.site.register(OrderItem)
-admin.site.register(Order)
+admin.site.register(Order, OrderAdmin)
 admin.site.register(Payment)
