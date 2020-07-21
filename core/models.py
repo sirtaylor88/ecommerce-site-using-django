66269bfdbde268da9897a8add823af89c21b51de
Nhat Tai NGUYEN <nhattai.nguyen88@gmail.com>
2020-07-21 18:54:39 +0200

    Update ref_code to be optional

--- core/models.py
+++ core/models.py
@@ -78,7 +78,9 @@ class OrderItem(models.Model):
 class Order(models.Model):
     user                = models.ForeignKey(settings.AUTH_USER_MODEL,
                                             on_delete=models.CASCADE)
-    ref_code            = models.CharField(max_length=20)
+    ref_code            = models.CharField(max_length=20,
+                                           blank=True,
+                                           null=True)
     items               = models.ManyToManyField(OrderItem)
     start_date          = models.DateTimeField(auto_now_add=True)
     ordered_date        = models.DateTimeField()
